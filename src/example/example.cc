0459b71c96a6b8c0da82f22883495d7ecdb3dabd
LXYan2333 <z00823823@126.com>
2023-12-12 22:57:06 +0800

    cpp example's dynamic reduce test

--- src/example/example.cc
+++ src/example/example.cc
@@ -1,31 +1,46 @@
+#include "simple_parallel/simple_parallel.h"
 #include <cstddef>
 #include <fmt/core.h>
+#include <fmt/ranges.h>
 #include <mpi.h>
-#include <simple_parallel/advance.h>
-#include <simple_parallel/simple_parallel.h>
-#include <tbb/tbb.h>
+#include <simple_parallel/main.h>
 #include <vector>
 
-auto virtual_main(int argc, char** argv) -> int {
+auto main() -> int {
     fmt::print("Hello from virtual main\n");
     size_t i = 4;
     std::vector<size_t> a = {1, 2, 3, 4};
     simple_parallel::broadcast_stack_and_heap();
-    simple_parallel::print_memory_on_worker(a.data(), 4);
-    simple_parallel::run_lambda([&]() {
-        fmt::print("Hello{} from {}", i, MPI::COMM_WORLD.Get_rank());
-    });
-    return 0;
-}
 
-auto main(int argc, char** argv) -> int {
-    // std::cin.get();
-    // initialize your global variable here
+    fmt::print("Hello{} from {}\n", i, MPI::COMM_WORLD.Get_rank());
+    fmt::print("a is {}\n", a);
 
-    // after this line, your modification on global variables won't be synced to
-    // other processes, and you can only access global variables directly (i.e.
-    // you should't access global variable through pointers)
-    simple_parallel::init(virtual_main, argc, argv, true);
+    if (MPI::COMM_WORLD.Get_rank() != 0) {
+        return 0;
+    }
 
+    fmt::print("=================\nstart reduce\n=================\n");
+    int reduce_result{};
+    simple_parallel::dynamic_schedule_reduce(
+        0,
+        110,
+        20,
+        [] { return 0; },
+        [](const int& start, const int& end, const int& identity) -> int {
+            fmt::println("rank{} start {} end {} identity {}",
+                         MPI::COMM_WORLD.Get_rank(),
+                         start,
+                         end,
+                         identity);
+            return identity + (start + end - 1) * (end - start) / 2;
+        },
+        [&](const int& identity) {
+            fmt::println("sum is {}", identity);
+            MPI::COMM_WORLD.Reduce(
+                &identity, &reduce_result, 1, MPI::INT, MPI::SUM, 0);
+            return identity;
+        });
+
+    fmt::println("reduce result is {}", reduce_result);
     return 0;
 }
