4c144323978cc9f5719c0a45b848041e2c7b1a01
LXYan2333 <z00823823@126.com>
2023-12-12 22:47:37 +0800

    build system and IDE config update

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -8,11 +8,6 @@ function(simple_parallel_setup_dependencies)
     find_package(MPI REQUIRED)
     message(STATUS "Run: ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${MPIEXEC_MAX_NUMPROCS} ${MPIEXEC_PREFLAGS} EXECUTABLE ${MPIEXEC_POSTFLAGS} ARGS")
 
-    # find_package(hwloc REQUIRED)
-    find_package(PkgConfig REQUIRED)
-    pkg_check_modules(HWLOC REQUIRED IMPORTED_TARGET hwloc)
-
-
     # For each dependency, see if it's
     # already been provided to us by a parent project
 
@@ -31,56 +26,6 @@ function(simple_parallel_setup_dependencies)
     endif()
     if (bigmpi_ADDED)
         target_include_directories(bigmpi INTERFACE ${bigmpi_SOURCE_DIR}/src)
-    endif()
-
-    if(NOT TARGET CLI11::CLI11)
-        CPMAddPackage("gh:CLIUtils/CLI11@2.3.2")
-    endif()
-
-    if(NOT TARGET ittnotify)
-        CPMAddPackage(
-            NAME
-            ittnotify
-            VERSION
-            3.24.0
-            GITHUB_REPOSITORY
-            "intel/ittapi")
-    endif()
-
-    if(NOT TARGET GTest)
-        CPMAddPackage(
-        NAME googletest
-        VERSION 1.14.0
-        GITHUB_REPOSITORY "google/googletest"
-        OPTIONS "gtest_force_shared_crt ON")
-    endif()
-
-    if(NOT TARGET TBB)
-        # set(BUILD_SHARED_LIBS ON)
-        CPMAddPackage(
-            NAME TBB
-            VERSION 2021.10.0
-            GITHUB_REPOSITORY "oneapi-src/oneTBB"
-            OPTIONS
-            "TBB_TEST OFF"
-            "TBBMALLOC_BUILD OFF"
-            "TBBMALLOC_PROXY_BUILD OFF"
-            "BUILD_SHARED_LIBS ON")
-    endif()
-
-    if(NOT TARGET cppcoro)
-        CPMAddPackage(
-            NAME cppcoro
-            GIT_TAG a3082f56ba135a659f7386b00ff797ba441207ba
-            GITHUB_REPOSITORY "andreasbuhr/cppcoro"
-            OPTIONS "BUILD_TESTING OFF")
-    endif()
-  
-    if(NOT TARGET range-v3)
-        CPMAddPackage(
-            NAME range-v3
-            GIT_TAG 0.12.0
-            GITHUB_REPOSITORY "ericniebler/range-v3")
     endif()
 
     if(NOT TARGET fmt::fmt)
