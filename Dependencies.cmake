80ceb5984c0ff8ccf7f80a169d9bb2ed76899f63
LXYan2333 <z00823823@126.com>
2023-12-27 21:25:45 +0800

    using PackageProject to package project, add Boost::mpi dependence

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -8,40 +8,51 @@ function(simple_parallel_setup_dependencies)
     find_package(MPI REQUIRED)
     message(STATUS "Run: ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${MPIEXEC_MAX_NUMPROCS} ${MPIEXEC_PREFLAGS} EXECUTABLE ${MPIEXEC_POSTFLAGS} ARGS")
 
-    # For each dependency, see if it's
-    # already been provided to us by a parent project
+    CPMAddPackage("gh:TheLartians/PackageProject.cmake@1.11.1")
+
+    # add Boost::mpi
+    set(TRY_BOOST_VERSION "1.84.0")
+    set(BOOST_NOT_HEADER_ONLY_COMPONENTS_THAT_YOU_NEED "mpi")
+    # set(BOOST_HEADER_ONLY_COMPONENTS_THAT_YOU_NEED "")
+
+    set(IS_BOOST_LOCAL OFF)
+    if(simple_parallel_FORCE_FIND_PACKAGE)
+        message(STATUS "Trying to find Boost...")
+        find_package(Boost ${TRY_BOOST_VERSION} REQUIRED COMPONENTS
+            ${BOOST_NOT_HEADER_ONLY_COMPONENTS_THAT_YOU_NEED})
+        set(IS_BOOST_LOCAL ON)
+    elseif(${CPM_USE_LOCAL_PACKAGES} OR NOT ${CPM_DOWNLOAD_ALL})
+        message(STATUS "Trying to use local Boost...")
+        find_package(Boost ${TRY_BOOST_VERSION} COMPONENTS ${BOOST_NOT_HEADER_ONLY_COMPONENTS_THAT_YOU_NEED})
+        if(${BOOST_FOUND})
+            set(IS_BOOST_LOCAL ON)
+            message(DEBUG "boost include dir: ${Boost_INCLUDE_DIRS}")
+        endif()
+    endif()
+
+    if(NOT (${BOOST_FOUND}) OR (NOT DEFINED BOOST_FOUND))
+        message(STATUS "Trying to download Boost...")
+        set(BOOST_INCLUDE_LIBRARIES "mpi")
+        CPMAddPackage(
+            NAME    Boost
+            URL     "https://github.com/boostorg/boost/releases/download/boost-${TRY_BOOST_VERSION}/boost-${TRY_BOOST_VERSION}.tar.xz"
+            OPTIONS "BOOST_ENABLE_MPI ON")
+        set(IS_BOOST_LOCAL OFF)
+    endif()
 
     if(simple_parallel_FORCE_FIND_PACKAGE)
         find_package(mimalloc CONFIG REQUIRED)
         find_package(Microsoft.GSL CONFIG REQUIRED)
     else()
-        find_package(mimalloc CONFIG)
-        if(NOT mimalloc_FOUND)
-            CPMAddPackage(
-                NAME mimalloc
-                GITHUB_REPOSITORY "microsoft/mimalloc"
-                VERSION 2.1.2)
-            install(TARGETS mimalloc
-                    EXPORT  simple_parallelTargets
-                    LIBRARY     DESTINATION ${CMAKE_INSTALL_LIBDIR}
-                    ARCHIVE     DESTINATION ${CMAKE_INSTALL_LIBDIR}
-                    RUNTIME     DESTINATION ${CMAKE_INSTALL_BINDIR}
-                    INCLUDES    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-        endif()
-
-        find_package(Microsoft.GSL CONFIG)
-        if(NOT Microsoft.GSL_FOUND)
-            CPMAddPackage(
-                NAME GSL
-                GITHUB_REPOSITORY "Microsoft/GSL"
-                VERSION 4.0.0
-                OPTIONS "GSL_INSTALL ON")
-            install(TARGETS GSL
-                    EXPORT  simple_parallelTargets
-                    LIBRARY     DESTINATION ${CMAKE_INSTALL_LIBDIR}
-                    ARCHIVE     DESTINATION ${CMAKE_INSTALL_LIBDIR}
-                    RUNTIME     DESTINATION ${CMAKE_INSTALL_BINDIR}
-                    INCLUDES    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-        endif()
+        CPMAddPackage(
+            NAME mimalloc
+            GITHUB_REPOSITORY "microsoft/mimalloc"
+            VERSION 2.1.2)
+        CPMAddPackage(
+            NAME GSL
+            GITHUB_REPOSITORY "Microsoft/GSL"
+            VERSION 4.0.0
+            OPTIONS "GSL_INSTALL ON")
     endif()
+
 endfunction()
